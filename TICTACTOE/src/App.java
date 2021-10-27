2e291b0a8179fe0f187a4ae43a44dc42d2129520
Anish Thakur <real.anish9@gmail.com>
2021-10-27 13:28:09 -0500

    array filled and printed

--- TICTACTOE/src/App.java
+++ TICTACTOE/src/App.java
@@ -1,5 +1,24 @@
+import java.util.*;
+
 public class App {
+
+    public static void printTable(String array[][]){
+        for(int i = 0; i<3; i++){
+            for(int j = 0; j<3; j++){
+                System.out.print(array[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) throws Exception {
-        
+        String [][] table=new String[3][3];
+
+        // fill the array with 0
+        for(int i = 0; i<3; i++){
+            for(int j = 0; j<3; j++){
+                table[i][j]="0";
+            }
+        }
+        printTable(table);
     }
 }
