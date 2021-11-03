4b8348d31929a09d98f821582c7319c623c9be32
Anish <real.anish9@gmail.com>
2021-11-03 14:46:56 -0700

    Added new features

--- TICTACTOE/src/com/kuebikoit/ticTac/handler/PlayerHandlerIf.java
+++ TICTACTOE/src/com/kuebikoit/ticTac/handler/PlayerHandlerIf.java
@@ -1,9 +1,10 @@
 package com.kuebikoit.ticTac.handler;
 
+import com.kuebikoit.ticTac.models.RowCol;
 import com.kuebikoit.ticTac.players.UserPlayer;
 
 public interface PlayerHandlerIf {
 
     boolean isTurn(UserPlayer userPlayer);
-    void move(UserPlayer userPlayer, int row, int col);
+    void move(UserPlayer userPlayer, RowCol rowCol);
 }
